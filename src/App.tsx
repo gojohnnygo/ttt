cbba337d2a84fe29101e2b6be9c1f47090a9083e
Johnny Nguyen <hellojohnnguyen@gmail.com>
2021-02-28 21:53:16 -0800

    init Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useRef} from 'react';
+
+import Board, {BoardRef} from './ttt/Board';
+
+import './App.css';
 
 function App() {
+  const boardRef = useRef<BoardRef>(null);
+
   return (
-    <div></div>
+    <div className="app">
+      <Board ref={boardRef}/>
+      <div>
+        <button onClick={() => boardRef.current?.reset()}>
+          Reset Default
+        </button>
+        <button
+          onClick={() => boardRef.current?.increment()}
+        >
+          Increase Board Size
+        </button>
+        <button
+          onClick={() => boardRef.current?.decrement()}
+        >
+          Decrease Board Size
+        </button>
+      </div>
+    </div>
   );
 }
 
